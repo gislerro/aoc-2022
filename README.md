705749692e31d50f9b69b2cfa269013989619785
gislerro <gislerro@student.ethz.ch>
2022-12-03 18:11:12 +0100

    days 1-3

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 AOC 2022 Solution written in Rust
+
+
+## Downloading today's input
+`cargo aoc input`
+
+## Solving a puzzle
+### Writing a generator (parser)
+A single annotated method which can return a custom type:
+```
+#[aoc_generator(dayX)]
+pub fn input_generator(input: &str) -> Vec<T> {
+   ...
+}
+```
+can either take in a `&str` or `&[u8]`
+
+### Writing a solver
+Takes in a slice of the parsed input and solves the (sub) task:
+```
+#[aoc(dayX, partX)]
+pub fn solve_part1(input: &[T]) -> u32 {
+   ...
+}
+```
+
+## Running your solution
+`cargo aoc` to run the latest implemented day (and downloading the input file if needed)
+`cargo aoc -d {day} -p {part}` for older days
+
+## Benchmarking
+`cargo aoc bench -o` to open the benchmark result directly in the browser
---
+++ src/day3.rs
@@ -0,0 +1,58 @@
+use itertools::Itertools;
+
+
+type Compartments = (String, String);
+
+pub fn get_priority(c: char) -> u32 {
+   (c as u32) - if c.is_ascii_uppercase() { 64 - 26 } else { 96 }
+}
+
+#[aoc_generator(day3)]
+pub fn parse_rucksack<'a>(input: &'a str) -> Vec<Compartments> {
+   input
+      .lines()
+      .map(|l| {
+         let mid = l.len() / 2;
+         (l[..mid].to_owned(), l[mid..].to_owned())
+      })
+      .collect()
+}
+
+#[aoc(day3, part1)]
+pub fn solve_part1(input: &[Compartments]) -> u32 {
+   input
+      .iter()
+      .map(|(left, right)| {
+         let dup = left.chars().filter(|c| right.contains(*c)).next().unwrap();
+         get_priority(dup)
+      })
+      .sum()
+}
+
+#[aoc(day3, part2)]
+pub fn solve_part2(input: &[Compartments]) -> u32 {
+   input
+      .iter()
+      .map(|(l, r)| format!("{}{}", l, r))
+      .tuples::<(_, _, _)>()
+      .map(|(a, b, c)| {
+         let badge = a.chars().filter(|ac| b.contains(*ac)).filter(|abc| c.contains(*abc)).next().unwrap();
+         get_priority(badge)
+      })
+      .sum()
+   
+}
+
+
+#[cfg(test)]
+mod tests {
+   use super::*;
+
+   #[test]
+   fn check_priority() {
+      assert_eq!(get_priority('a'), 1);
+      assert_eq!(get_priority('z'), 26);
+      assert_eq!(get_priority('A'), 27);
+      assert_eq!(get_priority('Z'), 52);
+   }
+}
